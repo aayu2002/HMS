4662213e21be26020cadf41756dee8d620738825
aayu2002 <91870028+aayu2002@users.noreply.github.com>
2024-03-18 15:01:01 +0530

    Update PaymentDAO.java

--- com/hospital/dao/PaymentDAO.java
+++ com/hospital/dao/PaymentDAO.java
@@ -1,10 +1,13 @@
 package com.hospital.dao;
 
 import com.hospital.entity.Payment;
+import com.hospital.entity.Bill;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
 
 public class PaymentDAO {
     private SessionFactory sessionFactory;
@@ -13,16 +16,31 @@ public class PaymentDAO {
         sessionFactory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Payment.class)
+                .addAnnotatedClass(Bill.class)
                 .buildSessionFactory();
     }
 
-    public void addPayment(Payment payment) {
+    public void addPayment(Payment payment, String billId) {
         Session session = sessionFactory.openSession();
         Transaction transaction = null;
 
         try {
             transaction = session.beginTransaction();
+
+            // Retrieve the Bill object from the database or create a new one
+            Bill bill = session.get(Bill.class, billId);
+            if (bill == null) {
+                // Create a new Bill object if it doesn't exist
+                bill = new Bill();
+                session.save(bill);
+            }
+
+            // Associate the Bill object with the Payment object
+            payment.setBill(bill);
+
+            // Save the Payment object
             session.save(payment);
+
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
@@ -33,6 +51,9 @@ public class PaymentDAO {
             session.close();
         }
     }
+
+
+
 
     public void updatePayment(Payment payment) {
         Session session = sessionFactory.openSession();
@@ -92,5 +113,27 @@ public class PaymentDAO {
         }
 
         return payment;
+    }
+    public List<Payment> getAllPayments() {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = null;
+        List<Payment> payments = null;
+
+        try {
+            transaction = session.beginTransaction();
+            payments = session.createQuery("FROM Payment", Payment.class).getResultList();
+            transaction.commit();
+        } catch (Exception e) {
+            // Rollback transaction in case of exception
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        } finally {
+            // Close session
+            session.close();
+        }
+
+        return payments;
     }
 }
